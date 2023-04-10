3b209d6c86a311abf7d9dab4251ad53376fca889
Aprikot <mandani850@gmail.com>
2023-04-10 14:00:33 +0700

    init flutter framework

---
+++ sociomile-flutter-sdk-ios/README.md
@@ -0,0 +1,3 @@
+# sociomile-flutter-sdk-ios
+
+A description of this package.
---
+++ sociomile-flutter-sdk-ios/Sources/sociomile-flutter-sdk-ios/sociomile_flutter_sdk_ios.swift
@@ -0,0 +1,6 @@
+public struct sociomile_flutter_sdk_ios {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
